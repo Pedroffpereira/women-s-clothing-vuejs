3879a13dcc7bd51a99711670102968fcee561511
Vasco Silva <vasco.silva.t0123187@edu.atec.pt>
2023-12-12 03:21:21 +0000

    fixed the addToCart function again

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,12 +1,30 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { defineStore } from 'pinia';
+import { ref, computed } from 'vue';
+import { useCouponStore } from './coupon'
+
 
 export const useCartStore = defineStore('cart', () => {
-    const items = ref([])
+    const items = ref([]);
+    const totalPrice = computed(() => {
+      /*let sum = 0;
+      items.forEach(item => {
+        sum += item.price * item.quantity
+      });
+      return sum;*/
+      return 19.3494;
+    })
+
+    const couponStore = useCouponStore();
+    const discountPrice = computed(() => totalPrice.value * couponStore.discount);
     
     function addToCart(obj) {
-      items.push(obj)
+      items.value.push(obj);
     }
   
-    return { items, addToCart }
+    return {
+      items,
+      totalPrice,
+      discountPrice,
+      addToCart,
+    }
   })
