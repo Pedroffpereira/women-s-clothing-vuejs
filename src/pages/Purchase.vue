86fe9be32035bd974a22cd1f3f97ada53ac32d37
Vasco Silva <vasco.silva.t0123187@edu.atec.pt>
2024-01-01 22:18:07 +0000

    added containers to purchase and cart

--- src/pages/Purchase.vue
+++ src/pages/Purchase.vue
@@ -40,15 +40,17 @@ onUnmounted(() => cartStore.purchase = {});
 </script>
 
 <template>
-    <div class="d-grid justify-content-center">
-        <div v-if="cartStore.purchase.success">
-            <h1 class="mb-5 text-center">Thank you for your purchase!</h1>
-            <Receipt></Receipt>
-        </div>
-        <Error :message="errorMessage" v-else-if="hasError"></Error>
-        <Error :message="cartStore.purchase.error" v-else></Error>
-        <div class="text-center">
-            <ReturnToShop class="mt-5"></ReturnToShop>
+    <div class="container py-5 my-5">
+        <div class="d-grid justify-content-center">
+            <div v-if="cartStore.purchase.success">
+                <h1 class="mb-5 text-center">Thank you for your purchase!</h1>
+                <Receipt></Receipt>
+            </div>
+            <Error :message="errorMessage" v-else-if="hasError"></Error>
+            <Error :message="cartStore.purchase.error" v-else></Error>
+            <div class="text-center">
+                <ReturnToShop class="mt-5"></ReturnToShop>
+            </div>
         </div>
     </div>
 </template>
