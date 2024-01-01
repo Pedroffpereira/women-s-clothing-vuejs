74560391541b28930683cdce5d05377b2867ad0b
Vasco Silva <vasco.silva.t0123187@edu.atec.pt>
2024-01-01 22:12:34 +0000

    fixed cart

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,28 @@ import Navbar from './components/navbar.vue'
 import { useProductsList } from '../src/store/products.js'
 import { mapState, mapActions } from 'pinia'
 import { saveProductsCache } from './api/products.js'
+import { setCart } from './api/cart'
+import { useCartStore } from '../src/store/cart'
 export default {
   components: {
     Footer,
     Navbar,
   },
   async mounted() {
-
     const cache = await saveProductsCache()
     this.addDate(cache.Date);
     this.addProductList(cache.Products)
+
+    const cart = setCart(cache.Products)
+    this.addCart(cart)
   },
   computed: {
     ...mapState(useProductsList, ['date', 'products']),
+    ...mapState(useCartStore, ['items']),
   },
   methods: {
-    ...mapActions(useProductsList, ['addDate', 'addProductList'])
+    ...mapActions(useProductsList, ['addDate', 'addProductList']),
+    ...mapActions(useCartStore, ['addCart'])
   }
 }
 
--- src/api/cart.js
+++ src/api/cart.js
@@ -1,4 +1,5 @@
 import { post } from '../helpers/request_helpers/'
+import { setInLocalStorage, getFromLocalStorage, removeFromLocalStorage } from '../helpers/local_storage_helpers.js'
 
 async function apiMakePurchase(checkout) {
     try {
@@ -8,4 +9,62 @@ async function apiMakePurchase(checkout) {
     }
 }
 
-export { apiMakePurchase };
+function setCart(products) {
+    const localStorageCart = getFromLocalStorage('cart');
+    
+    const validatedCart = validateCart(localStorageCart, products);
+    
+    if (validatedCart.length > 0) setInLocalStorage('cart', validatedCart);
+    else removeFromLocalStorage('cart');
+
+    return validatedCart;
+}
+
+function validateCart(cart, products) {
+    let empty = false;
+    try {
+      if (!cart || !Array.isArray(cart)) {
+        empty = true;
+        return;
+      }
+      
+      if (cart.length === 0) {
+        empty = true;
+        return;
+      }
+
+      if (products.length === 0) {
+        empty = true;
+        return;
+      }
+
+      for (let index = 0; index < cart.length; index++) {
+        const item = cart[index];
+        const product = products.find(p => p.id === item.id);
+        
+        if (!product) {
+          cart.splice(index, 1);
+          continue;
+        }
+        
+        if (product.quantity === 0) {
+          cart.splice(index, 1);
+          continue;
+        }
+
+        if (product.quantity !== item.quantity) {
+          item.quantity = product.quantity;
+          if (item.cartQuantity > item.quantity) {
+            item.cartQuantity = item.quantity;
+          }
+        }
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      empty = true;
+    } finally {
+      return empty ? [] : cart;
+    }
+}
+
+export { apiMakePurchase, setCart };
